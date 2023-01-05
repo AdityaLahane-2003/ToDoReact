883b57b5483a319547677c7253489469686acde2
Aditya Lahane <adityalahane31@gmail.com>
2023-01-05 18:15:24 +0530

    Created Build

---
+++ build/precache-manifest.92b0acb586095b1d80e38e36f9b41ee8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651c2ade32038c27b368b92d600a8d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7441b963c4cd19361e97",
+    "url": "/static/js/2.71074f19.chunk.js"
+  },
+  {
+    "revision": "feb80abdaee7228a34e2",
+    "url": "/static/js/main.13be24ab.chunk.js"
+  },
+  {
+    "revision": "2b39c98d96c9238cdd2e",
+    "url": "/static/js/runtime-main.28f67ebe.js"
+  }
+]);
